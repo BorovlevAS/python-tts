b2712f1ae3a50fc56de04246a5e3b1bea6c05e65
BorovlevAS <bas@qdes.com.ua>
2022-05-11 02:03:09 +0300

    update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from cmath import inf
 from pathlib import Path
 from pdfminer import high_level
+from gtts import gTTS
 
-def process_file(file_name, lang):
+def process_file(file_name, language):
 
     path = Path(file_name)
 
@@ -10,15 +10,24 @@ def process_file(file_name, lang):
         print(f"Can't process {file_name}")
         return
     
+    print('Start reading file...')
+
     with open(file_name, 'rb') as fpdf:
         text = high_level.extract_text(fpdf)
 
-    with open('text.txt', 'w', encoding='UTF-8') as fwr:
-        fwr.write(text.replace('\n', ''))
+    text = text.replace('\n', '')
+    tts = gTTS(text, lang = language)
+
+    print('Generating mp3...')
+    fname = path.name.replace(path.suffix, '') + '.mp3'
+    with open(fname, 'wb') as fmp3:
+        tts.write_to_fp(fmp3)
+
+    print(f'File {fname} was generated!')
     
 
 def main():
-    process_file('F:\\python-projects\\vscode-test\\PDF_Converter_Pro_Quick_Reference_Guide.RU.pdf', 'en')
+    process_file('F:\\python-projects\\vscode-test\\PDF_Converter_Pro_Quick_Reference_Guide.RU.pdf', 'ru')
 
 if __name__ == '__main__':
     main()
